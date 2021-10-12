0ffe8b726c9b92dcac383edcd66277c2149bd89b
TheOpti <wieckowski.piotr.93@gmail.com>
2021-10-12 23:43:43 +0200

    Add unit tests for /me endpoint

--- src/routes/logout/index.test.ts
+++ src/routes/logout/index.test.ts
@@ -9,6 +9,6 @@ describe('/logout endpoint', () => {
 		expect(res.get('set-cookie')).toEqual(['token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly']);
 		expect(res.get('cookie')).toEqual(undefined);
 		expect(res.status).toEqual(200);
-		expect(res.body.message).toEqual(RESPONSE_MESSAGES.LOG_OUT_OK,);
+		expect(res.body.message).toEqual(RESPONSE_MESSAGES.LOG_OUT_OK);
 	});
 });
